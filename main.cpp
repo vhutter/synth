a97345ec1804b816e6fabf3b810a56dc5203eeb1
Hütter Vince <vince_rock777@yahoo.com>
2019-04-29 00:39:17 +0200

    Test configuration created in VS, test branch should be removed from now

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 #include "synthMain.h"
 
+#ifdef __TEST__
+#include "test/testMain.h"
+#else
+#include "synthMain.h"
+#endif
+
 int main(int argc, char** argv)
 {
+#ifdef __TEST__
+	testMain(argc, argv);
+#else
 	synthMain(argc, argv);
+#endif
 }
